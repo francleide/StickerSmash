11507f30d15cf9d71f473540b5fc941cc13d36e3
francleide <francleide7@gmail.com>
2024-03-22 19:13:42 -0300

    division into components

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image} from 'react-native';
 
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
 const Image1 = require('./assets/imagens/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style ={styles.imageContainer}>
-          <Image source ={Image1} style={styles.image}/>
+         < ImageViewer placeholderImageSource={Image1}/>
+        </View>
+        <View style={styles.footerContainer}>
+          <Button label="Escolha uma foto"/>
+          <Button label="Use essa foto"/>
         </View>
       <StatusBar style="auto" />
     </View>
@@ -21,13 +28,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     //justifyContent: 'center',
   },
-  imageContainer:{
+  imageContainer: {
     flex: 1,
     paddingTop: 58,
   },
-  image:{
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+ /* image:{
     width: 320,
     height: 440,
     borderRadius: 18,
-  },
+  },*/
 });
