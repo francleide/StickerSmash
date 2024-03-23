2611da68260acc5e1753c678e2a1f0ebc3384b4e
francleide <francleide7@gmail.com>
2024-03-23 14:26:51 -0300

    added gestures

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View} from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -50,7 +51,7 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style ={styles.imageContainer}>
          < ImageViewer 
          placeholderImageSource={Image1}
@@ -76,7 +77,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
